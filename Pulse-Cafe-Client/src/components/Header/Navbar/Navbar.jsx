44146a05a33d88c1aca4508601532ee37e8945f8
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-25 19:46:46 +0600

    added active navlink style in navbar

--- Pulse-Cafe-Client/src/components/Header/Navbar/Navbar.jsx
+++ Pulse-Cafe-Client/src/components/Header/Navbar/Navbar.jsx
@@ -1,21 +1,23 @@
 import { Link, NavLink } from "react-router-dom";
 
 import logo from "../../../assets/images/logo.png";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          {/* <a className=" text-xl">Pulse Cafe</a> */}
-          <Link className="flex items-center gap-2">
-            <img className="w-12" src={logo} />
-            <h1>Pulse Cafe</h1>
+          <Link to={"/"} className="flex items-center gap-2">
+            <img className="w-10 mt-1" src={logo} />
+            <h1 className="text-4xl font-bold">Pulse Cafe</h1>
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <NavLink>Add Cofee</NavLink>
+            <NavLink to={"/addCoffee"} className="font-semibold text-xl">
+              Add Cofee
+            </NavLink>
           </ul>
         </div>
         <div className="navbar-end">
