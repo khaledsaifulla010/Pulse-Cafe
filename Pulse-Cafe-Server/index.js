3d4cbde9fbd13f266ca83b13475dfed1774f21dc
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-25 22:30:59 +0600

    get all coffee from server using get methods

--- Pulse-Cafe-Server/index.js
+++ Pulse-Cafe-Server/index.js
@@ -43,6 +43,14 @@ async function run() {
       res.send(result);
     });
 
+    //----------------------GET NEW COFFEE IN MONGODB--------------------------//
+
+    app.get("/coffee", async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
