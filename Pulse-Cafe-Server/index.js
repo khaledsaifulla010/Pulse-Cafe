d395de7314837ffa2a84250bf73a08accc03b4ee
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-11-25 22:07:05 +0600

    post a new coffee in the server using post method

--- Pulse-Cafe-Server/index.js
+++ Pulse-Cafe-Server/index.js
@@ -6,16 +6,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middlewares //
+//-------------------------------Middlewares-------------------------------------//
 
 app.use(cors());
 app.use(express.json());
 
-
-//  CONNECT WITH MONGODB //
+//------------------------CONNECT WITH MONGODB------------------------------------//
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ugbxhsw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +27,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    //---------------------MAKE A DATABASE COLLECTION IN MONGODB----------------//
+
+    const coffeeCollection = client.db("coffeeStore").collection("coffee");
+
+    //----------------------POST A NEW COFFEE IN MONGODB-------------------------//
+
+    app.post("/coffee", async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -37,12 +50,12 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-// Server Running //
+//--------------------------------Server Running--------------------------------//
 
 app.get("/", (req, res) => {
   res.send("Pulse Cafe Server is Running");
